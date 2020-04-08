08c6ec1dd3eee70a2461a3c681bedfd67b0793bb
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2020-04-08 16:25:12 +1000

    feat(auth): add amplify authenticator with sign in/out

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import Amplify from "aws-amplify";
+import aws_exports from "./aws-exports";
+import "@aws-amplify/ui/dist/style.css";
+Amplify.configure(aws_exports);
 
 ReactDOM.render(
   <React.StrictMode>
