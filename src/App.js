852dfeebffca689428ea57a710ff90491c7ddb41
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2020-04-09 08:04:01 +1000

    feat: add static notes and input box

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import { withAuthenticator } from "aws-amplify-react";
 
-function App() {
-  return <div>App!</div>;
+class App extends Component {
+  state = {
+    notes: [
+      {
+        id: 1,
+        note: "Hello World"
+      }
+    ]
+  };
+  render() {
+    const { notes } = this.state;
+    return (
+      <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+        <h1 className="code f2-1">Amplify Notetaker</h1>
+        <form className="mb3">
+          <input type="text" className="pa2 f4" placeholder="write your note" />
+          <button className="pa2 f4" type="submit">
+            Add Note
+          </button>
+        </form>
+
+        {/* Notes List */}
+        {notes.map(item => (
+          <div key={item.id} className="flex items-center">
+            <li className="list pa1 f3">{item.note}</li>
+            <button className="bg-transparent bn f4">
+              <span>&times;</span>
+            </button>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default withAuthenticator(App, { includeGreetings: true });
