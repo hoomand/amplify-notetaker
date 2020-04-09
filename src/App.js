544dc5b4672f5ba496174b27b167726178972499
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2020-04-09 18:18:05 +1000

    feat(createNote): add graphql to save note

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React, { Component } from "react";
+import { API, graphqlOperation } from "aws-amplify";
+import { createNote } from "./graphql/mutations";
 import { withAuthenticator } from "aws-amplify-react";
 
 class App extends Component {
   state = {
-    notes: [
-      {
-        id: 1,
-        note: "Hello World"
-      }
-    ]
+    note: "",
+    notes: []
   };
+
+  handleChangeNote = event => {
+    this.setState({ note: event.target.value });
+  };
+
+  handleAddNote = event => {
+    event.preventDefault();
+    const { note } = this.state;
+    API.graphql(graphqlOperation(createNote, { input: { note } }));
+  };
+
   render() {
     const { notes } = this.state;
     return (
       <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
         <h1 className="code f2-1">Amplify Notetaker</h1>
-        <form className="mb3">
-          <input type="text" className="pa2 f4" placeholder="write your note" />
+        <form onSubmit={this.handleAddNote} className="mb3">
+          <input
+            type="text"
+            className="pa2 f4"
+            placeholder="write your note"
+            onChange={this.handleChangeNote}
+          />
           <button className="pa2 f4" type="submit">
             Add Note
           </button>
